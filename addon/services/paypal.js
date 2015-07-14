6651c9bb50ebfbe63958ae358b10be3176f4644c
Joseph Dillon <joseph.dillon.522@gmail.com>
2015-07-14 11:06:56 -0400

    updated data passing

--- addon/services/paypal.js
+++ addon/services/paypal.js
@@ -3,12 +3,30 @@ import Token from './token';
 
 export default Ember.Service.extend({
 	
-	submitPayment: function() {
+	submitPayment: function(opt) {
 		var token = window.localStorage.pptoken;
+		var auth = 'Bearer ';
+		var data = { 'grant_type' : 'client_credentials' };
+		var path = '/v1/payments/payment';
+
 
 		if (!token) {
-			token = Token.getToken();	
-		}
+			token = Token.getToken(opt);				
+		} 
+
+		auth = 'Bearer ' + token;
+
+		Ember.$.ajax({
+			url: 'https://api.sandbox.paypal.com' + path,
+			method: 'GET',
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader('Authorization', auth);
+			},
+			data: data,
+			success: function(success) {
+				console.log('PAYMENTS', success)
+			}
+		});
 		
 	}
 });
--- addon/services/token.js
+++ addon/services/token.js
@@ -4,8 +4,8 @@ import Utils from '../utils';
 var clientID = 'AYZnZPw0ztH2YZARlYelfsBqQbB3sW6UDxqPeHgD8Jxg7z-f59Pz9CrQMUn_6TgyiQtyNLsQJz2C1BDi';
 var secret = 'EJSTRmMNrPs-pYjUNCsi5u_HVBZTDZiVSYGCn_B2SOVbB1PE0u-kqsJs_EUpUOggaxI8_5PhnxbT6Vz3';
 
-export function getToken(options) {
-	var auth = 'Basic ' + btoa(clientID + ':' + secret);
+export function getToken(opt) {
+	var auth = 'Basic ' + btoa(opt.clientID || clientID + ':' + opt.secret || secret);
 	var data = { 'grant_type' : 'client_credentials' };
 	var path = '/v1/oauth2/token';
 
